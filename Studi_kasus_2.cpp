8f23e3e065bc052f1e97c92547cc8edde398b201
yudahasta22 <101837652+yudahasta22@users.noreply.github.com>
2022-03-30 12:09:28 +0700

    Update Studi_kasus_2.cpp

--- Studi_kasus_2.cpp
+++ Studi_kasus_2.cpp
@@ -1,94 +1,98 @@
 #include <iostream>
 #include <fstream>
 using namespace std;  
-        
+
 int main() {  
-  fstream myFile;
+	fstream myFile;
 	myFile.open("Struk.txt", ios::out);
-  	long ayamgeprek, ayamgoreng, udanggoreng, cumigoreng, ayambakar, biaya, diskon, biayatotal, bayar, kembalian;  
-  	int jumlah;  
-  	char a,b,c,d,e,paket;  
-   
+  	long bakar,goreng,geprek,udang,cumi, biaya, diskon, biayatotal, bayar, kembalian;  
+  	int jumlah,jarak,pajakjarak;  
+  	char a,b,c,d,e;  
+  	
+
   	cout<<"-------------------------"<<endl;  
   	cout<<"  WARUNG AYAM   "<<endl;  
   	cout<<"-------------------------"<<endl<<endl;  
-   
+
   	cout<<"MENU PAKET:       "<<endl;  
-  	cout<<"A. Ayam Geprek : Rp. 21000 "<<endl;  
-  	cout<<"B. Ayam Goreng  : Rp. 17000 "<<endl;  
-  	cout<<"C. Udang Goreng  : Rp. 19000 "<<endl;
-  	cout<<"D. Cumi Goreng  : Rp. 20000 "<<endl;
-  	cout<<"E. Ayam Bakar  : Rp. 25000 "<<endl;
- 
+  	cout<<"1. Ayam Geprek  : Rp. 21000 "<<endl;  
+  	cout<<"2. Ayam Goreng  : Rp. 17000 "<<endl;
+  	cout<<"3. Udang Goreng : Rp. 19000 "<<endl;
+  	cout<<"4. Cumi Goreng  : Rp. 20000 "<<endl;
+  	cout<<"5. Ayam Bakar   : Rp. 25000 "<<endl;
+
+
 	cout<<"Pesan Ayam Geprek ?(y/t) ";
 	cin>>a;
-	
+
 	if (a=='y') {
 		cout<<"Masukkan Jumlah Pesanan : ";
-		cin>>ayamgeprek;
-		cout<<"Jumlah Harga  = "<<ayamgeprek*21000<<endl;
+		cin>>geprek;
+		cout<<"Jumlah Harga  = "<<geprek*21000<<endl;
 	}
 	else {
-		cout<<"Pesanan Ayam geprek = 0\n";
-		ayamgeprek=0;
+		cout<<"Pesanan Ayam Geprek = 0\n";
+		geprek=0;
 	}
-	
-	cout<<"Pesan Ayam goreng  ?(y/t) ";
+	cout<<"Pesan Ayam Goreng ?(y/t) ";
 	cin>>b;
+
 	if (b=='y') {
 		cout<<"Masukkan Jumlah Pesanan : ";
-		cin>>ayamgoreng;
-		cout<<"Jumlah Harga  = "<<ayamgoreng*17000<<endl;
+		cin>>goreng;
+		cout<<"Jumlah Harga  = "<<goreng*17000<<endl;
 	}
 	else {
-		cout<<"Pesanan Ayam goreng  = 0\n";
-		ayamgoreng=0;
+		cout<<"Pesanan Ayam Goreng = 0\n";
+		goreng=0;
 	}
 	
-	cout<<"Pesan udang goreng  ?(y/t) ";
+	
+	cout<<"Pesan Udang Goreng ?(y/t) ";
 	cin>>c;
+
 	if (c=='y') {
 		cout<<"Masukkan Jumlah Pesanan : ";
-		cin>>udanggoreng;
-		cout<<"Jumlah Harga  = "<<udanggoreng*19000<<endl;
+		cin>>udang;
+		cout<<"Jumlah Harga  = "<<udang*19000<<endl;
 	}
 	else {
-		cout<<"Pesanan udang goreng  = 0\n";
-		udanggoreng=0;
+		cout<<"Pesanan Udang Goreng = 0\n";
+		udang=0;
 	}
-	
-	cout<<"Pesan cumi goreng  ?(y/t) ";
+	cout<<"Pesan Cumi Goreng ?(y/t) ";
 	cin>>d;
+
 	if (d=='y') {
 		cout<<"Masukkan Jumlah Pesanan : ";
-		cin>>cumigoreng;
-		cout<<"Jumlah Harga  = "<<cumigoreng*20000<<endl;
+		cin>>cumi;
+		cout<<"Jumlah Harga  = "<<cumi*20000<<endl;
 	}
 	else {
-		cout<<"Pesanan cumi goreng  = 0\n";
-		cumigoreng=0;
+		cout<<"Pesanan Ayam Goreng = 0\n";
+		cumi=0;
 	}
 	
-	cout<<"Pesan ayam bakar  ?(y/t) ";
+	cout<<"Pesan Ayam Bakar  ?(y/t) ";
 	cin>>e;
 	if (e=='y') {
 		cout<<"Masukkan Jumlah Pesanan : ";
-		cin>>ayambakar;
-		cout<<"Jumlah Harga  = "<<ayambakar*25000<<endl;
+		cin>>bakar;
+		cout<<"Jumlah Harga  = "<<bakar*17000<<endl;
 	}
 	else {
-		cout<<"Pesanan ayam bakar  = 0\n";
-		ayambakar=0;
+		cout<<"Pesanan Ayam Bakar  = 0\n";
+		bakar=0;
 	}
-	
-	 	biaya=(geprek*21000)+(goreng*17000)+(udang*19000)+(cumi*20000)+(bakar*21000);
+
+ 	biaya=(geprek*21000)+(goreng*17000)+(udang*19000)+(cumi*20000)+(bakar*21000);
 	cout<<"Total Pesanan : "<<endl;
 	cout<<"1. Ayam Geprek  : "<<geprek*21000<<endl;
 	cout<<"2. Ayam Goreng  : "<<goreng*17000<<endl;
 	cout<<"3. Udang Goreng : "<<udang*19000<<endl;
   	cout<<"4. Cumi Goreng  : "<<cumi*20000<<endl;
 	cout<<"5. Ayam Bakar   : "<<bakar*21000<<endl;
-	cout<<"_____________"<<endl;
+	cout<<"_____________________________________"<<endl;
 	cout<<"Total Harga    : "<<biaya<<endl;
 	
 	cout<<"Masukkan Jarak Pengantaran : ";
@@ -113,8 +117,44 @@ int main() {
 		diskon=0.35*biaya+(pajakjarak-8000);
 		
 	}  
- 	 biayatotal=biaya-diskon;
+ 	 biayatotal=biaya-diskon; 
 	
 	
+
+  	cout<<"-------------------------"<<endl;  
+  	cout<<"  WARUNG AYAM   "<<endl;  
+  	cout<<"-------------------------"<<endl<<endl;  
+
+	cout<<"RINCIAN PEMBAYARAN:   "<<endl;  
+  	cout<<"1. Ayam Goreng  : "<<goreng*17000<<endl;
+	cout<<"2. Ayam Bakar   : "<<bakar*21000<<endl; 
+	cout<<"3. Udang Goreng : "<<udang*19000<<endl;
+  	cout<<"4. Cumi Goreng  : "<<cumi*20000<<endl;
+	cout<<"5. Ayam Bakar   : "<<bakar*21000<<endl; 
+  	cout<<"         ______________________ x"<<endl;  
+  	cout<<"Biaya      	   = "<<biaya<<endl;
+	cout<<"Jarak		   = "<<jarak<<endl;
+  	cout<<"Diskon          = "<<diskon<<endl;  
+  	cout<<"         ______________________ -"<<endl;  
+  	cout<<"Biaya Total     = "<<biayatotal<<endl;  
+  	cout<<"Bayar     	   = "; cin>>bayar; kembalian=bayar-biayatotal;  
+  	cout<<"         ______________________"<<endl;  
+  	cout<<"Uang Kembalian  = "<<kembalian<<endl; 
+
+	myFile<<"Nota Pembayaran:   "<<endl;  
+    	myFile<<"1. Ayam Goreng  : "<<goreng*17000<<endl;
+	myFile<<"2. Ayam Bakar   : "<<bakar*21000<<endl; 
+	myFile<<"3. Udang Goreng : "<<udang*19000<<endl;
+  	myFile<<"4. Cumi Goreng  : "<<cumi*20000<<endl;
+	myFile<<"5. Ayam Bakar   : "<<bakar*21000<<endl; 
+  	myFile<<"         ______________________ x"<<endl;  
+  	myFile<<"Biaya      	 = "<<biaya<<endl;
+	myFile<<"Jarak		     = "<<jarak<<endl;
+  	myFile<<"Diskon          = "<<diskon<<endl;  
+  	myFile<<"         ______________________ -"<<endl;  
+  	myFile<<"Biaya Total     = "<<biayatotal<<endl;  
+  	myFile<<"Bayar     	     = "<<bayar; kembalian=bayar-biayatotal;  
+  	myFile<<"         ______________________"<<endl;  
+  	myFile<<"Uang Kembalian  = "<<kembalian<<endl;  
 	return 0;
 }
